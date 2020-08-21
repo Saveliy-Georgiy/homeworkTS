95c932f645a152b5d850ab5b128835d50120316a
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2020-08-21 16:17:28 +0300

    Done homework7

--- src/components/MainComponents/Junior/Junior.tsx
+++ src/components/MainComponents/Junior/Junior.tsx
@@ -2,6 +2,10 @@ import React, {useState} from 'react';
 import styles from './Junior.module.css'
 import {MyEditableSpan} from "../../common/MyEditableSpan/MyEditableSpan";
 import {restoreState, saveState} from "../../../brouserFunctions/brouserFunctions";
+import {MySelect} from '../../common/MySelect/MySelect';
+import {MyRadio} from "../../common/MyRadio/MyRadio";
+import {v1} from "uuid";
+import {MyButton} from "../../common/MyButton/MyButton";
 
 type StateType = {
     title: string
@@ -12,7 +16,6 @@ const Junior = () => {
     const [title, setTitle] = useState("я обычный спан")
 
     const onChangeHandler = (value: string) => {
-        debugger
         setTitle(value)
     }
 //* использование:
@@ -26,12 +29,38 @@ const Junior = () => {
         setTitle(state.title)
     }
 // получем в переменную state объект из ячейки "test" или дэфолтный объект если ячейка пуста
-    
+
+    const [selected, setSelected] = useState('2');
+
+    const [radioSelected, setRadioSelected] = useState("Email");
+
     return (
         <div className={styles.mainContainer}>
-            <MyEditableSpan title={title} placeholder="напиши здесь что-нибудь" onChange={onChangeHandler}/>
-            <button onClick={saveTitle}>save value</button>
-            <button onClick={restoreTitle}>restore value</button>
+            <div className={styles.mainWrapper}>
+            <MyEditableSpan title={title} placeholder="write something here" onChange={onChangeHandler}/>
+            <MyButton onClick={saveTitle} name="save value"/>
+            <MyButton onClick={restoreTitle} name="restore value"/>
+            </div>
+            <div>
+                <span>Choose your city</span>
+            <MySelect value={selected}
+                      items={[
+                          {value: "1", title: "Moscow"},
+                          {value: "2", title: "Kiev"},
+                          {value: "3", title: "Minsk"}
+                      ]}
+                      onChange={setSelected}/>
+            </div>
+            <span>Please select your preferred contact method:</span>
+            <MyRadio
+                name="contacts"
+                value={radioSelected}
+                onChange={setRadioSelected}
+                items={[
+                    {id: v1(), title: "Email"},
+                    {id: v1(), title: "Phone"},
+                    {id: v1(), title: "Mail"}
+                ]}/>
         </div>
     );
 }
---
+++ src/components/common/MyRadio/MyRadio.tsx
@@ -0,0 +1,44 @@
+import React, {useEffect, useState} from "react";
+import styles from './MyRadio.module.css'
+
+type ItemType = {
+    title: string
+    id: string
+}
+
+type RadioPropsType = {
+    value: string
+    onChange: (value: string) => void
+    items: ItemType[]
+    name: string
+}
+
+export function MyRadio(props: RadioPropsType) {
+
+    const onItemClick = (value: string) => {
+        props.onChange(value);
+    }
+
+    return (
+        <div>
+            {
+                <div>
+                    {
+                        props.items.map(i => {
+                            return <div>
+                                <input
+                                    type="radio"
+                                    name={props.name}
+                                    key={i.id}
+                                    value={props.value}
+                                    onClick={() => {onItemClick(i.title)}}>
+                                </input>
+                                <span>{i.title}</span>
+                            </div>
+                        })
+                    }
+                </div>
+            }
+        </div>
+    )
+}
